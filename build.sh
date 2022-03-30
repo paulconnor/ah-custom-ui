cd96fb9acfea4f28d3bfcd61b3acf3d17e30fce5
paulconnor <paul.connor21@gmail.com>
2022-03-30 17:02:15 +1100

    Delete build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-docker build -f Dockerfile.auth -t pconnor/custom-ui-auth:latest .
-docker build -f Dockerfile.cms -t  pconnor/custom-ui-cms:latest .
-docker push pconnor/custom-ui-cms:latest
-docker push pconnor/custom-ui-auth:latest
