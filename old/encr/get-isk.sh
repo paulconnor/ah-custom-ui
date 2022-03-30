3d68015a2d6d235f3095736fed28e530c4416898
paulconnor <paul.connor21@gmail.com>
2022-03-30 16:48:41 +1100

    Delete old directory

--- old/encr/get-isk.sh
+++
@@ -1,8 +0,0 @@
-(kubectl get secrets -n ssp ssp-ssp-keys-isk -o jsonpath="{.data.tls\.key}" | base64 --decode; echo) > isk.key
-(kubectl get secrets -n ssp ssp-ssp-keys-isk -o jsonpath="{.data.tls\.crt}" | base64 --decode; echo) > isk.crt
-
-#(kubectl get secrets -n ssp ssp-ssp-keys-mek -o jsonpath="{.data.tls\.key}" | base64 --decode; echo) > mek.key
-#(kubectl get secrets -n ssp ssp-ssp-keys-mek -o jsonpath="{.data.tls\.crt}" | base64 --decode; echo) > mek.crt
-
-openssl x509 -in isk.crt -text
-openssl rsa -in isk.key -text
